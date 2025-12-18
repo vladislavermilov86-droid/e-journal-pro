b6af5404c702ff50a21dc28fc75585c34d2dfa76
Vladislav <vladigra121@gmail.com>
2025-12-18 17:21:45 +0500

    1701

--- api/boards.ts
+++ api/boards.ts
@@ -1,6 +1,6 @@
 
 import { VercelRequest, VercelResponse } from '@vercel/node';
-import sql from './db';
+import sql from './db.js';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   try {
